0afb2983a5516c57b165981e669b73460076539f
Daku Gamer <dakugamer21@gmail.com>
2025-07-09 04:57:56 +0000

    invalid credentials gimme what  added ??

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDV3FND7OcdVdSI9_eYeLBqEITbhAGLFZk",
-  authDomain: "jagdishodedara-35423.firebaseapp.com",
-  projectId: "jagdishodedara-35423",
-  storageBucket: "jagdishodedara-35423.appspot.com",
-  messagingSenderId: "558612594123",
-  appId: "1:558612594123:web:524434efb500231657ca4e",
-  measurementId: "G-NNSCSVMCJR"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
